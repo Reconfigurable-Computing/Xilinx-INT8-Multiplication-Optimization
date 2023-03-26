ddf5f185dea7763ea420c97e899cba9fd41f8bc3
Vinci <louwenqi@mail.ustc.edu.cn>
2023-03-26 12:03:53 +0800

    Update int8_mult.cpp

--- INT8-HLS/src/int8_mult.cpp
+++ INT8-HLS/src/int8_mult.cpp
@@ -2,9 +2,12 @@
 
 ap_int<32> MUL_MAC(ap_int<8> A, ap_int<8> W0, ap_int<8> W1)
 {
+    // method-1, note that (W0, ap_uint<16>(0)) not equals the ((ap_int<25>)w0)<<16, so this method is wrong 
     //ap_int<24> W;
     //W = (W0, ap_uint<16>(0)) + ap_int<24>(W1);
-    // following method is better, lwq-fixed
+    
+    //method-2
+    // following method is right and better, lwq-fixed
     ap_int<25> W= W0;
     W <<= 16;
     W+=W1;
